f20aa64b9c22c17718b66dc20360e8c025329e83
saima <saiamqadri008@gmail.com>
2024-12-09 00:15:37 +0500

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import BlueHeader from "./components/blue-header";
+import Carausel from "./components/caraousel";
+import Editors from "./components/editorspick";
+// import Footer from "./components/footer";
+import GreenDiv from "./components/green-div";
+import Navbar from "./components/navbar";
+import ProductCard from "./components/products-card";
+import Whitediv from "./components/white-dic";
+
+export default function Home(){
+  return(
+    <div>
+      <BlueHeader/>
+      <Navbar/>
+      <Carausel/>
+      <Editors/>
+      <ProductCard/>
+      <GreenDiv/>
+      <Whitediv/>
+      {/* <Footer/> */}
+           
+    </div>
+  )
+}
